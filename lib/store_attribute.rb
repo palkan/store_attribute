3db365ba5a9ad718f9ba1a44cc655103191109a7
Vladimir Dementyev <palkan@hey.com>
2023-03-07 19:26:04 -0500

    refactor: drop Configuration

--- lib/store_attribute.rb
+++ lib/store_attribute.rb
@@ -2,16 +2,6 @@
 
 require "store_attribute/version"
 require "store_attribute/active_record"
-require "store_attribute/configuration"
 
 module StoreAttribute
-  class << self
-    def configuration
-      @configuration ||= Configuration.new
-    end
-
-    def configure
-      yield(configuration)
-    end
-  end
 end
